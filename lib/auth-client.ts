e1e74cdb759741b5867b2775f7da68551f6f6473
Mark Ramos <mark.p.ramos@gmail.com>
2026-02-08 15:50:29 +0000

    use Vercel URL if available

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,7 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.BETTER_AUTH_URL,
 });
